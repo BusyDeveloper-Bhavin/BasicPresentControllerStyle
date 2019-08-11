e05e3a39a6ff61bb34c7c9c0462aa0eba2a30bae
Bhavin <bhavin.vaghela292@gmail.com>
2019-08-11 21:34:37 +0530

    Set all basic setups

--- BasicPresentStyle/Controllers/BaseListVC.swift
+++ BasicPresentStyle/Controllers/BaseListVC.swift
@@ -9,25 +9,31 @@
 import UIKit
 
 class BaseListVC: UIViewController {
-
+    
+    //MARK:- Variables
     var animator : VBAnimator = VBAnimator()
     
+    //MARK:- Action Methods
     @IBAction func btnProgramaticallyCLicked(_ sender: Any) {
         guard let obj : PresentOverCurrentVC = self.storyboard?.instantiateViewController(withIdentifier: "PresentOverCurrentVC") as? PresentOverCurrentVC else { return }
-        obj.transitioningDelegate = animator
+        // obj.transitioningDelegate = animator
         obj.modalTransitionStyle = .crossDissolve
         obj.modalPresentationStyle = .overFullScreen //use full screen insted of overCurrentContext
         self.present(obj, animated: true, completion: nil)
     }
+    
+    //MARK:- Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    
+        
     }
-
+    
+    //MARK:- Segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.transitioningDelegate = animator
+        // segue.destination.transitioningDelegate = animator
     }
 }
 
